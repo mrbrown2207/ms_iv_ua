d380330ed51b16edfb73685a39bfec3c3c1350c0
Michael Brown <michael.brown@fundzzle.com>
2020-03-01 12:27:53 +0000

    Added issue status change code

--- issue/views.py
+++ issue/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from home.constants import FILTERS
 from .models import Issue
 from .forms import IssueForm
 
@@ -37,3 +38,31 @@ def upvote(request, id):
     # Take user back to main page, but ensure we maintain the filter state
     return redirect(reverse('filters', args=(request.session['issue_filter'],)))
 
+
+def updissstatus(request, id):
+    """
+    Change the status. This will advance the status by one.
+    So, if an issue has been reported and we come into this
+    function, then it will then get promoted to "ongoing".
+    If it is ongoing and we come in here, then will be promoted
+    to "fixed". This could have easily been combined with the
+    upvote function, but did this for complete clarity.
+    """
+    i = get_object_or_404(Issue, pk=id)
+
+    # This should not happen, but check to make sure that we are
+    # not here for an item with the wrong status. Also, make
+    # sure the record doesn't already have a closed status. Again,
+    # it shouldn't happen, but let's be safe.
+    if i.status >= FILTERS.get('issues_closed'):
+        messages.add_message(request, messages.ERROR, "Issue is already closed.")
+    else:
+        x = i.status + 1
+        if x > FILTERS.get('issues_closed'):
+            messages.add_message(request, messages.ERROR, "Action results in an invalid status.")
+        else:
+            i.status = x
+            i.save()
+
+    # Take user back to main page, but ensure we maintain the filter state
+    return redirect(reverse('filters', args=(request.session['issue_filter'],)))
